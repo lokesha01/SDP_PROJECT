33c2a54c107cf4e19c9216ae3c33c6572bf8e594
lokesha01 <118422183+lokesha01@users.noreply.github.com>
2024-08-14 20:08:34 +0530

    Update SociographyApplication.java

--- backend/src/main/java/com/example/sociography/SociographyApplication.java
+++ backend/src/main/java/com/example/sociography/SociographyApplication.java
@@ -3,7 +3,6 @@ package com.example.sociography;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-// import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class SociographyApplication {
